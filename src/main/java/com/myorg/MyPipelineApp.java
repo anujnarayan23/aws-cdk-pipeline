a2358fdb2d4cc784e037db8dd7e45c5dbad7e36a
Narayan <anunarayan@deloitte.com>
2021-11-10 16:48:24 +0530

    initial commit first

---
+++ src/main/java/com/myorg/MyPipelineApp.java
@@ -0,0 +1,20 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
+
+public class MyPipelineApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new MyPipelineStack(app, "PipelineStack", StackProps.builder()
+            .env(new Environment.builder()
+                .account("876757926184")
+                .region("us-east-1")
+                .build())
+            .build());
+
+        app.synth();
+    }
+}
